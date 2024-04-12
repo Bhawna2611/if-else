21f038e56ef61940fef61bdf493a17f9f483d02a
bhawnadangarh <bhawnadandarh@gmail.com>
2024-04-12 19:03:32 +0530

    push if_else to git

---
+++ if_else.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "please enter your age:"
+read age
+if [ "$age" -lt '18' ]
+then
+	echo "sorry, you are not old enough to vote."
+elif [ "$age" -ge '18' ] && [ "$age" -lt 65 ] 
+then
+	echo "you are eligible to vote."
+else
+	echo "you are eligiable to vote, and you are also eligiable for senior benifits."
+fi
